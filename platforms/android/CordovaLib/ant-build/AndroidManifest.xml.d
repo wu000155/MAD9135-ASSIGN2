6e7d4381b180f7c4f8f55249cbf995fca6d2faa5
warrenbutland <warrenbutland@Warrens-MacBook-Pro.local>
2014-10-21 19:16:15 -0400

    platform

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes.dex : \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/HelloCordova.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/R$attr.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/R$drawable.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/R$string.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/R$xml.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/classes/io/cordova/hellocordova/R.class \
+/Users/warrenbutland/MAD9135-ASSIGN2/platforms/android/ant-build/dexedLibs/classes-e7c16dcbbbefca445d2f87a356d7ac5d.jar \
