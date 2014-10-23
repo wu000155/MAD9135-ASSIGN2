246b004e11cc37f6093be842cd2bc7e9605fb255
warrenbutland <warrenbutland@Warrens-MacBook-Pro.local>
2014-10-23 19:58:20 -0400

    some html and javascript requests working

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -34,18 +34,109 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        
+        //console.log("Device ready");        
+// jquery not working
+
+        //var clickMe = document.querySelector(".clickme");
+
+
+  
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function() {
+         
+       $("#downloadPhoneMusic").each( function() {
+            alert("yehaa");
+            console.log("Inside alert function");
+            });
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        $('#home').addClass(".show");
+        $('#two').addClass(".hide");
+        
+        //console.log('Received Event: ' + id);
+        
+        app.downloadMusic();
+    },
 
-        console.log('Received Event: ' + id);
-    }
-};
+   
+
+    downloadMusic: function(){
+
+        var request = new XMLHttpRequest();
+        request.open("GET", "http://feeds.feedburner.com/ThrillingAdventureHour");
+        console.log(request);
+            
+        request.onreadystatechange = function() {
+                if (request.readyState == 4) {
+                    if (request.status == 200 || request.status == 0) {
+                        var music = request.responseXML;
+                       
+                        console.log(music);
+
+                        var event = document.createEvent('Event');
+                        event.initEvent('contactLocationReady', true, true);
+                        document.dispatchEvent(event);
+                        
+                    }
+                }
+            }
+        request.send();
+    },// end of getmusic
+
+
+
+
+    gotMusic: function(){
+        return this.success;   
+        transferDownloaded();
+    },
+
+     errorCallback: function(error) {
+      alert(error.code);
+    },
+
+    
+
+    transferDownloaded: function(){
+
+/*
+        var uri = encodeURI("");
+           
+        fileTransfer.download(uri, fileURL, function(entry) 
+        {
+                console.log("download complete: " + entry.toURL());
+        }
+        
+        function(error) {
+                    console.log("download error code" + error.code);
+        }
+            false,
+            { 
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+
+            }
+        */
+
+       /* var transfer = new FileTransfer();
+        transfer.download(source, target, successCallback, errorCallback);*/
+
+
+     /* $("#clickme").on('click' function(){
+       // remove show and add hide to page 1
+        $('#home').removeClass(.show);
+        $('#home').addClass(.hide);
+        // remove hide and add show to page 2
+         $('#two').removeClass(.hide);
+        $('#two').addClass(.show);*/
+        
+
+      },
+
+}
 
 app.initialize();
+
+
